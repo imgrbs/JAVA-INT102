5ab0ed3b9eaf9efa48241d116a19da0b9b68aa17
ImagineRabbits <mixxdarkz2@gmail.com>
2016-10-27 17:40:34 +0700

    Tue Java 27-10-2559

---
+++ RegularPolygon.java
@@ -0,0 +1,55 @@
+class RegularPolygon{
+	private int n;
+	private double side;
+	private double x;
+	private double y;
+	
+	public RegularPolygon(){
+		
+	}
+	public RegularPolygon(int n, double side){
+		this.n=n;
+		this.side=side;
+	}
+	public RegularPolygon(int n , double side , double x , double y){
+		this.n=n;
+		this.side=side;
+		this.x=x;
+		this.y=y;
+	}
+	public void setN(int valuen){
+		n = valuen;
+	}
+	public int getN(){
+		return n;
+	}
+	
+	public void setSide(double valueSide){
+		side = valueSide;
+	}
+	public double getSide(){
+		return side;
+	}
+	
+	public void setX(double valueX){
+		x = valueX;
+	}
+	public double getX(){
+		return x;
+	}
+	
+	public void setY(double valueY){
+		y = valueY;
+	}
+	public double getY(){
+		return y;
+	}
+	public double getParameter(){
+		double parameter= n*side;
+		return parameter;
+	}
+	public double getArea(){
+		double area= (n*(side*side))/ 4* (Math.tan(Math.PI/n));
+		return area;
+	}
+}
---
+++ TestRegularPolygon.java
@@ -0,0 +1,7 @@
+class TestRegularPolygon{
+	public static void main(String[] args){
+		RegularPolygon rp = new RegularPolygon(4,4);
+		System.out.println(rp.getParameter());
+		System.out.println(rp.getArea());
+	}
+}
