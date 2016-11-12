f1d49c341ad525a21cc486fa31b12855b1f48135
IMGRBS <mixxdarkz2@gmail.com>
2016-11-12 17:30:53 +0700

    ข้อ2 post 8

--- Post11/Pen.java
+++ Post11/Pen.java
@@ -4,7 +4,34 @@ class Pen{
 	private String brand;
 	private double price;
 	private static double VAT = 0.07;
-		
+	private static int count;
+	private static String print;
+	
+	public Pen(){
+		this(0);
+	}
+	public Pen(long productId){
+		this(productId,null);
+	}
+	public Pen(long productId,String model){
+		this(productId,model,null);
+	}
+	public Pen(long productId,String model,String brand){
+		this(productId,model,brand,0);
+	}
+	public Pen(long productId,String model,String brand,double price){
+		this.productId=productId;
+		this.model=model;
+		this.brand=brand;
+		this.price=price;
+		count++;
+		print = print+"Pen id "+id+" model "+model+" "+brand+" "+price+"\n";
+	}
+	
+	public static int numberOfPen(){
+		return count;
+	}
+	
 	public void setProductId(long pid){
 		productId=pid;
 	}
@@ -49,10 +76,15 @@ class Pen{
 			return "difference Brand";
 	}
 	
+	public static String allPen(){
+		return print;
+	}
+	
 	public String toString(){
 		return "Product ID : "+productId+
 			   "\nModel : "+model+
 			   "\nBrand : "+brand+
 			   "\nPrice : "+price;
 	}
+
 }
